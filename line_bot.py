b6c2ee3c1c2f70e357c084b452bbdbe6d97cdf2a
s012xx <s012xx@yahoo.co.jp>
2025-12-19 15:49:47 +0900

    Update line_bot.py

--- line_bot.py
+++ line_bot.py
@@ -382,4 +382,6 @@ def handle_image(event):
 # 起動
 # ==============================
 if __name__ == "__main__":
-    app.run(port=5000, debug=False)
+    port = int(os.environ.get("PORT", "5000"))
+    app.run(host="0.0.0.0", port=port, debug=False)
+    
